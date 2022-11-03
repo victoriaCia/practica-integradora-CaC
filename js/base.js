97d01093203ba604cd6c02b418df39bc0b2d6e33
victoriaCia <107944947+victoriaCia@users.noreply.github.com>
2022-11-03 20:49:37 -0300

    agrego signup sucursales clientes y showdata

---
+++ js/base.js
@@ -0,0 +1,58 @@
+let header=`
+    <div class="header__title">
+        <h1>La Cocina de Juan</h1>
+    </div>
+    <nav class="navbar navbar__container" id="navbar">
+        <div class="nav__title">
+            <p>La Cocina de Juan</p>
+        </div>
+        <input type="checkbox" id="toggler">
+        <label for="toggler"><i class="ri-menu-line"></i></label>
+        <div class="menu">
+        <ul class="list">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">Acerca de</a></li>
+            <li><a href="contact.html">Contacto</a></li>
+            <li><a href="signup.html">Registrarse</a></li>
+            <li><a href="sucursales.html">Sucursales</a></li>
+            <li><a href="clientes.html">Nuestros Clientes</a></li>
+        </ul>
+        </div>
+    </nav>
+    `
+
+let footer=`
+        <p>Victoria Cia</p>
+        <div class="footer__icons">
+            <p>Repositorio </p>
+            <a href="https://github.com/victoriaCia/primera_pagina_curso_codoacodo.git" target="_blank"><img src="media/assets/Github.svg"></a>
+        </div>
+        <p>Derechos reservados @2022</p>       
+    `
+
+
+
+document.getElementById('header').innerHTML=header;
+
+document.getElementById('footer').innerHTML=footer;
+
+
+
+ // When the user scrolls the page, execute myFunction
+ window.onscroll = function() {myFunction()};
+
+ // Get the navbar
+ let navbar = document.getElementById("navbar");
+
+ // Get the offset position of the navbar
+ let sticky = navbar.offsetTop;
+
+ // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+ function myFunction() {
+     console.log('llama funcion')
+     if (window.pageYOffset >= sticky) {
+         navbar.classList.add("sticky");
+     } else {
+         navbar.classList.remove("sticky");
+     }}
+
